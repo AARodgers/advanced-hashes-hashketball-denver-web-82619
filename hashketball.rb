b375d04337e93fa454ff9323671a7fa0181366d2
Amanda Rodgers <aarodgerswork@gmail.com>
2019-08-12 20:56:56 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,80 @@
-# Write your code here!
+require "pry"
 
+def game_hash
+  {
+    :home => 
+      { :team_name => 'Brooklyn Nets', 
+        :colors => ['Black', 'White'], 
+        :players => [
+          {player_name: 'Alan Anderson', 
+           number: 0, 
+           shoe: 16, 
+           points: 22, 
+           rebounds: 12, 
+           assists: 12, 
+           steals: 3, 
+           blocks: 1, 
+           slam_dunks: 1}, 
+          {player_name: 'Reggie Evans', 
+           number: 30, 
+           shoe: 14, 
+           points: 12, 
+           rebounds: 12, 
+           assists: 12, 
+           steals: 12, 
+           blocks: 12, 
+           slam_dunks: 7}, 
+           {player_name: 'Brook Lopez', 
+           number: 11, 
+           shoe: 17, 
+           points: 17, 
+           rebounds: 19, 
+           assists: 10, 
+           steals: 3, 
+           blocks: 1, 
+           slam_dunks: 15} 
+          ] },
+    
+      :away => 
+      { :team_name => 'Charlotte Hornets', 
+        :colors => ['Turquoise', 'Purple'], 
+        :players => [
+          {player_name: 'Jeff Adrien', 
+          number: 4, 
+          shoe: 18, 
+          points: 10, 
+          rebounds: 1, 
+          assists: 1, 
+          steals: 2, 
+          blocks: 7, 
+          slam_dunks: 2}, 
+          {player_name: 'Bismack Biyombo', 
+          number: 0, 
+          shoe: 16, 
+          points: 12, 
+          rebounds: 4, 
+          assists: 7, 
+          steals: 22, 
+          blocks: 15, 
+          slam_dunks: 10 }, 
+          {player_name: 'DeSagna Diop', 
+          number: 2, 
+          shoe: 14, 
+          points: 24, 
+          rebounds: 12, 
+          assists: 12, 
+          steals: 4, 
+          blocks: 5, 
+          slam_dunks: 5} 
+      ]} 
+  }
+end
+
+def num_points_scored(player_n)
+  game.hash.each do |team_data|
+   binding.pry
+ end
+end
 
 
 
