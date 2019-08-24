2251b4ced37ea4ff8e9b69f72a8d7d8c721195d3
Amanda Rodgers <aarodgerswork@gmail.com>
2019-08-24 16:45:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -166,24 +166,22 @@ def player_numbers(team_name)
   nums
 end
 
-def player_stats(sought_player_name)
+def player_stats(players_name)
   new_hash = {}
   
-  game_hash.collect do |place, team|
-    team.each do |attribute, _data|
-      next unless attribute == :players
-
-      game_hash[place][attribute].each do |player|
-        next unless player[:player_name] == sought_player_name
-
-        new_hash = player.delete_if do |k, _v|
-          k == :player_name
-        end
+  game_hash.each do |place team|
+    team.each do |attributes, data|
+      if attributes == :players
+        data.each do |player|
+      end
       end
     end
   end
-  new_hash
+      
+          
 end
+  
+
 
 def big_shoe_rebounds
   biggest_shoe = 0
